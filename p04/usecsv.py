b18ab2fb41b3052eb4dc76d6b0062525643bf08b
wi <whyeil@naver.com>
2026-09-03 21:50:55 +0900

    11

--- p04/usecsv.py
+++ p04/usecsv.py
@@ -14,3 +14,13 @@ def writecsv(filename, the_list):
     with open(filename, 'w', newline='', encoding='utf-8-sig') as f:
         a = csv.writer(f, delimiter=',')
         a.writerows(the_list)
+
+# 숫자 데이터만 뽑아 콤마(,) 제거
+def switch(listName):
+    for i in listName:
+        for j in i:
+            try:
+                i[i.index(j)] = float(j.replace(',', ''))
+            except:
+                pass
+    return listName
